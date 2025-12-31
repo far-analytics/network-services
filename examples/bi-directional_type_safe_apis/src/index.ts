38cc512a6d144e9ddbee28bc39d22c0006497d16
Adam Patterson <adpatter2000@gmail.com>
2025-12-31 13:08:23 -0500

    Format.

--- examples/bi-directional_type_safe_apis/src/index.ts
+++ examples/bi-directional_type_safe_apis/src/index.ts
@@ -1,95 +1,88 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import * as net from 'node:net';
-import { Async, createService } from 'network-services';
+import * as net from "node:net";
+import { Async, createService } from "network-services";
 
 // DataStore
 interface IDataStore {
-    addData: (...args: Array<number>) => Promise<void>;
-    getData: () => Array<number>;
+  addData: (...args: Array<number>) => Promise<void>;
+  getData: () => Array<number>;
 }
 
 class DataStore implements IDataStore {
+  private data: Array<number> = [];
+  private provider: Async<IDataProvider>;
+  private storageLimit: number = 10;
 
-    private data: Array<number> = [];
-    private provider: Async<IDataProvider>;
-    private storageLimit: number = 10;
+  constructor(provider: Async<IDataProvider>) {
+    this.provider = provider;
+  }
 
-    constructor(provider: Async<IDataProvider>) {
-        this.provider = provider;
+  async addData(...args: Array<number>): Promise<void> {
+    if (this.data.length < this.storageLimit) {
+      this.data = this.data.concat(args);
+    } else {
+      await this.provider.stop();
     }
+  }
 
-    async addData(...args: Array<number>): Promise<void> {
-        if (this.data.length < this.storageLimit) {
-            this.data = this.data.concat(args);
-        }
-        else {
-            await this.provider.stop();
-        }
-    }
+  getData(): Array<number> {
+    return this.data;
+  }
 
-    getData(): Array<number> {
-        return this.data;
-    }
-
-    throwError() {
-        throw new Error('Something wonderful happened.');
-    }
+  throwError() {
+    throw new Error("Something wonderful happened.");
+  }
 }
 
-const server = net.createServer().listen({ port: 3000, host: '127.0.0.1' });
-server.on('connection', (socket: net.Socket) => {
-    socket.on('error', console.error);
-    const service = createService(socket);
-    const provider = service.createServiceAPI<IDataProvider>();
-    const store = new DataStore(provider);
-    service.createServiceApp<IDataStore>(store, { paths: ['addData', 'getData'] });
+const server = net.createServer().listen({ port: 3000, host: "127.0.0.1" });
+server.on("connection", (socket: net.Socket) => {
+  socket.on("error", console.error);
+  const service = createService(socket);
+  const provider = service.createServiceAPI<IDataProvider>();
+  const store = new DataStore(provider);
+  service.createServiceApp<IDataStore>(store, { paths: ["addData", "getData"] });
 });
-
 
 // DataProvider
 interface IDataProvider {
-    start(): Promise<void>;
-    stop(): Promise<void>;
+  start(): Promise<void>;
+  stop(): Promise<void>;
 }
 
 class DataProvider implements IDataProvider {
+  public store: Async<IDataStore>;
+  public continue: boolean = true;
 
-    public store: Async<IDataStore>;
-    public continue: boolean = true;
+  constructor(store: Async<IDataStore>) {
+    this.store = store;
+  }
 
-    constructor(store: Async<IDataStore>) {
-        this.store = store;
+  async start() {
+    let n = 0;
+    while (this.continue) {
+      await this.store.addData(n++);
     }
+  }
 
-    async start() {
-        let n = 0;
-        while (this.continue) {
-            await this.store.addData(n++);
-        }
+  async stop() {
+    if (this.continue) {
+      this.continue = false;
+      const data = await this.store.getData();
+      console.log(data);
     }
-
-    async stop() {
-        if (this.continue) {
-            this.continue = false;
-            const data = await this.store.getData();
-            console.log(data);
-        }
-    }
+  }
 }
 
-const socket = net.connect({ port: 3000, host: '127.0.0.1' });
-socket.on('error', console.error);
-socket.on('ready', async () => {
-    try {
-        const service = createService(socket);
-        const store = service.createServiceAPI<IDataStore>();
-        const provider = new DataProvider(store);
-        service.createServiceApp(provider);
-        await provider.start();
-    }
-    catch (err) {
-        console.error(err);
-    }
+const socket = net.connect({ port: 3000, host: "127.0.0.1" });
+socket.on("error", console.error);
+socket.on("ready", async () => {
+  try {
+    const service = createService(socket);
+    const store = service.createServiceAPI<IDataStore>();
+    const provider = new DataProvider(store);
+    service.createServiceApp(provider);
+    await provider.start();
+  } catch (err) {
+    console.error(err);
+  }
 });
-
-
--- examples/scalable_hello_world/src/service.ts
+++ examples/scalable_hello_world/src/service.ts
@@ -1,14 +1,14 @@
-import { threadId } from 'node:worker_threads';
-import { createPortStream, createService } from 'network-services';
+import { threadId } from "node:worker_threads";
+import { createPortStream, createService } from "network-services";
 
 export class Greeter {
-    greet(kind: string) {
-        for (let now = Date.now(), then = now + 100; now < then; now = Date.now()); // Block for 100 milliseconds.
-        return `Hello, ${kind} world! from thread ${threadId}.`;
-    }
+  greet(kind: string) {
+    for (let now = Date.now(), then = now + 100; now < then; now = Date.now()); // Block for 100 milliseconds.
+    return `Hello, ${kind} world! from thread ${threadId}.`;
+  }
 }
 
 const greeter = new Greeter(); // Create an instance of the Greeter.
-const portStream = createPortStream(); // Create a PortStream that will wrap the `parentThread` MessagePort in a stream.Duplex. 
+const portStream = createPortStream(); // Create a PortStream that will wrap the `parentThread` MessagePort in a stream.Duplex.
 const service = createService(portStream); // Create a Service.
 service.createServiceApp(greeter); // Create a Service App.
